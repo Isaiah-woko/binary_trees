bff701797f1a325cb12c2c6e91c420358de96bfb
Isaiah woko <wokoisaiah68@gmail.com>
2024-04-11 16:11:36 +0100

    okay

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,10 +18,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	(*bt_new_node).parent = parent;
-	(*bt_new_node).n = value;
-	(*bt_new_node).left = NULL;
-	(*bt_new_node).right = NULL;
+	bt_new_node->parent = parent;
+	bt_new_node->n = value;
+	bt_new_node->left = NULL;
+	bt_new_node->right = NULL;
 
 	return (bt_new_node);
 }
